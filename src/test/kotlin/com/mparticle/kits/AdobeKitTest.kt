627a5e34bdf7f1b3d9886adf325f2baf22a12211
Mansi Pandya <mpandya@mparticle.com>
2026-01-14 17:24:38 -0500

    fix kotlin lint issue

--- src/test/kotlin/com/mparticle/kits/AdobeKitTest.kt
+++ src/test/kotlin/com/mparticle/kits/AdobeKitTest.kt
@@ -17,7 +17,6 @@ import java.util.Arrays
 import java.util.HashMap
 
 class AdobeKitTest {
-
     private val kit: AdobeKit
         get() = AdobeKit()
 
@@ -62,15 +61,16 @@ class AdobeKitTest {
 
     @Test
     fun testBuildUrl() {
-        val url = kit.encodeIds(
-            "<MCID>",
-            "<ORG ID>",
-            "<BLOB>",
-            "<REGION>",
-            "<PUSH TOKEN>",
-            "<GAID>",
-            HashMap(),
-        )
+        val url =
+            kit.encodeIds(
+                "<MCID>",
+                "<ORG ID>",
+                "<BLOB>",
+                "<REGION>",
+                "<PUSH TOKEN>",
+                "<GAID>",
+                HashMap(),
+            )
         val testUrl1 =
             "d_mid=<MCID>&d_ver=2&d_orgid=<ORG ID>&d_cid=20914%01<GAID>&d_cid=20919%01<PUSH TOKEN>&dcs_region=<REGION>&d_blob=<BLOB>&d_ptfm=android"
         assertEqualUnorderedUrlParams(url, testUrl1)
@@ -78,15 +78,16 @@ class AdobeKitTest {
         val userIdentities = HashMap<MParticle.IdentityType, String>()
         userIdentities[MParticle.IdentityType.CustomerId] = "<CUSTOMER ID>"
         userIdentities[MParticle.IdentityType.Email] = "<EMAIL>"
-        val url2 = kit.encodeIds(
-            "<MCID>",
-            "<ORG ID>",
-            "<BLOB>",
-            "<REGION>",
-            "<PUSH TOKEN>",
-            "<GAID>",
-            userIdentities,
-        )
+        val url2 =
+            kit.encodeIds(
+                "<MCID>",
+                "<ORG ID>",
+                "<BLOB>",
+                "<REGION>",
+                "<PUSH TOKEN>",
+                "<GAID>",
+                userIdentities,
+            )
         val testUrls2 =
             "d_mid=<MCID>&d_ver=2&d_orgid=<ORG ID>&d_cid=20914%01<GAID>&d_cid=20919%01<PUSH TOKEN>&dcs_region=<REGION>&d_blob=<BLOB>&d_ptfm=android&d_cid_ic=customerid%01<CUSTOMER ID>&d_cid_ic=email%01<EMAIL>"
         assertEqualUnorderedUrlParams(url2, testUrls2)
@@ -102,7 +103,8 @@ class AdobeKitTest {
             kit.kitManager.getIntegrationAttributes(
                 Mockito.any(KitIntegration::class.java),
             ),
-        ).thenReturn(integrationAttributes)
+        )
+            .thenReturn(integrationAttributes)
 
         val settings = HashMap<String, String>()
         settings[AdobeKitBase.AUDIENCE_MANAGER_SERVER] = "some.random.url"
@@ -112,18 +114,25 @@ class AdobeKitTest {
         assertEquals(url, "some.random.url")
     }
 
-    private fun assertEqualUnorderedUrlParams(url1: String?, url2: String?) {
+    private fun assertEqualUnorderedUrlParams(
+        url1: String?,
+        url2: String?,
+    ) {
         if (url1 == null && url2 == null) {
             return
         }
-        val url1Split = Arrays.asList(
-            *url1!!.split("&".toRegex()).dropLastWhile { it.isEmpty() }
-                .toTypedArray(),
-        )
-        val url2Split = Arrays.asList(
-            *url2!!.split("&".toRegex()).dropLastWhile { it.isEmpty() }
-                .toTypedArray(),
-        )
+        val url1Split =
+            Arrays.asList(
+                *url1!!.split("&".toRegex())
+                    .dropLastWhile { it.isEmpty() }
+                    .toTypedArray(),
+            )
+        val url2Split =
+            Arrays.asList(
+                *url2!!.split("&".toRegex())
+                    .dropLastWhile { it.isEmpty() }
+                    .toTypedArray(),
+            )
         assertEquals(url1Split.size.toLong(), url2Split.size.toLong())
         url1Split.sort()
         url2Split.sort()
